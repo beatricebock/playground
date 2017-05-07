37d6ec3067545841cff0559fec97ee0843612bd8
Tris <explaintotris@gmail.com>
2017-05-07 21:40:56 +0800

    moved responsive folder

--- vue/starbase-router/src/App.vue
+++ vue/starbase-router/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <h3>Starbase Router</h3>
+    <h4>
+      <router-link to="/data/people" class="link">Meet the Characters</router-link > | <router-link to="/data/planets" class="link">Explore the Planets</router-link>
+    </h4>
     <router-view></router-view>
   </div>
 </template>
---
+++ vue/starbase-router/src/components/Item.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="col-md-4">
+    <div class="item-card">
+      <div class="card-block">
+        <h4 class="card-title">{{item.name}}</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    props: ['item']
+  }
+</script>
